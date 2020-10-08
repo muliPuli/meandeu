25effaaae278623c86883a27fcd67b02c871f44c
muliPuli <56508119+muliPuli@users.noreply.github.com>
2020-10-08 10:20:59 +0200

    Delete javascript.js

--- javascript.js
+++
@@ -1,9 +0,0 @@
-$("body").hide();
-$("window").ready(function () {
-    $("body").fadeIn(1000);
-
-})
-let bgimg = document.querySelector("body");
-window.addEventListener("scroll", function () {
-    bgimg.style.backgroundPositionY = window.scrollY / 2 + "px";
-})
